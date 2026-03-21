f70c63c1c36e355b71a9a2a7c3279543140702b6
kininaru555-oss <kininaru555@gmail.com>
2026-03-21 19:52:50 +0900

    Update gacha-dom.js

--- gacha-dom.js
+++ gacha-dom.js
@@ -1,17 +1,21 @@
 export const drawButton = document.getElementById("drawButton");
 export const stage = document.getElementById("stage");
 export const placeholder = document.getElementById("placeholder");
+
 export const resultImage = document.getElementById("resultImage");
 export const resultVideo = document.getElementById("resultVideo");
+
 export const meta = document.getElementById("meta");
 export const ticketInfo = document.getElementById("ticketInfo");
 export const statusEl = document.getElementById("status");
 export const buybackPriceEl = document.getElementById("buybackPrice");
 export const featuredTypeEl = document.getElementById("featuredType");
+
 export const listingArea = document.getElementById("listingArea");
 export const listToMachineBtn = document.getElementById("listToMachineBtn");
 export const listingMessage = document.getElementById("listingMessage");
 export const listingFullBox = document.getElementById("listingFullBox");
+
 export const machineStatusBox = document.getElementById("machineStatusBox");
 export const mypageBtn = document.getElementById("mypageBtn");
 export const latestWorksBox = document.getElementById("latestWorks");
@@ -22,6 +26,9 @@ export const genreEl = document.getElementById("genre");
 export const descriptionEl = document.getElementById("description");
 export const mediaTypeEl = document.getElementById("mediaType");
 export const rarityEl = document.getElementById("rarity");
+
+export const resaleFromMypageBtn =
+  document.getElementById("resaleFromMypageBtn");
 
 export const jackpotEl = document.createElement("div");
 jackpotEl.id = "jackpot";
