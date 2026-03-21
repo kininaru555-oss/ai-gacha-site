785323ccd5c522c76e2fca5bdee29f5b24195c62
kininaru555-oss <kininaru555@gmail.com>
2026-03-21 17:11:54 +0900

    Update gacha-config.js

--- gacha-config.js
+++ gacha-config.js
@@ -1,7 +1,5 @@
-export const API_URL =
-  "https://script.google.com/macros/s/AKfycbzRW9dHBTOaRkM8gibA09i9L8zSiDDdd1YNWikqT4wn7zQWSaJQpo3-5CJx61od6-sNbQ/exec";
-
 export const API_BASE = "https://aicontents.onrender.com";
+export const CONTENTS_API = `${API_BASE}/api/contents`;
 export const OWNERSHIP_API = `${API_BASE}/api/ownerships`;
 export const POINT_API_BASE = `${API_BASE}/api/points`;
 export const VENDING_MACHINES_API = `${API_BASE}/api/vending-machines`;
