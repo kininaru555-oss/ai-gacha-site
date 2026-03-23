0d1f58aaf591c813d82fc53ff20fa9411c2bb34e
kininaru555-oss <kininaru555@gmail.com>
2026-03-23 13:46:46 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = {
+  API_BASE: "https://your-fastapi-service.onrender.com",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/xxxxxxxxxxxxxxxx/exec?token=YOUR_SECRET_TOKEN",
+  CLOUDINARY_CLOUD_NAME: "YOUR_CLOUD_NAME",
+  CLOUDINARY_UPLOAD_PRESET: "YOUR_UNSIGNED_PRESET",
+  AUTH_STORAGE_KEY: "gacha_app_auth_v1",
+  RESULT_STORAGE_KEY: "gacha_last_result",
+  POST_SUCCESS_NOTICE_KEY: "gacha_post_success_notice"
+};
